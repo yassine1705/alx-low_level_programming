d407fdbc358abf160864c7ec7af1a436aa8d0e7c
yassine1705 <yassine170505@gmail.com>
2023-03-16 17:40:42 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include "stdio.h"
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*  main - Entry point of the program
+*
+* This program prints the message "Programming is like building a
+* multilingual puzzle" to the standard output.
+*
+* Return: 0 on successful program execution
+*/
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
+
     return (0);
 }
