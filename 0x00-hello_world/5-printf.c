4ad50c9e697ac8e2097d6c870cc00710e941b5df
yassine1705 <yassine170505@gmail.com>
2023-03-16 18:12:11 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints a sentence that is grammatically correct and artistically poetic
- *
- * Description: This function uses the printf function to print the sentence
- * "with proper grammar, but the outcome is a piece of art," followed by a
- * newline character.
- *
- * Return: 0 (indicating success)
- */
+* main - Prints a sentence that is grammatically correct and artistically poetic
+*
+* Description: This function uses the printf function to print the sentence
+* "with proper grammar, but the outcome is a piece of art," followed by a
+* newline character.
+*
+* Return: 0 (indicating success)
+*/
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
