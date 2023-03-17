8a1c91b1caba8ebb5378832dcf32ca7068a20c75
yassine1705 <yassine170505@gmail.com>
2023-03-16 18:05:23 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main() {
-printf("From the deepest desires often come the deadliest hate, "
-           "but the outcome can also be a masterpiece.\n");
+/**
+* @brief Prints a sentence that is grammatically correct and artistically poetic.
+*
+* This function uses the printf function to print the sentence "with proper grammar, but the outcome is a piece of art,"
+* followed by a newline character.
+*
+* @return 0, indicating success.
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return 0;
 }
