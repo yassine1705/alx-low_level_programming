0a3e10dc3cd67bc214f346d749b170f8dbe041fd
yassine1705 <yassine170505@gmail.com>
2023-05-02 17:44:48 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - Prints a name
+  *
+  * Return: Nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
