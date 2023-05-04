4742d83764064e08069cbadebc6d4a2d27f68bb6
yassine1705 <yassine170505@gmail.com>
2023-05-03 17:17:58 -0700

    fix betty

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,6 @@
-/*
- * File: 0-puts_recursion.c
- * 
- */
-
 #include "main.h"
-
 /**
- * _puts_recursion Prints a string, followed by a new line.
+ * _puts_recursion - Prints a string, followed by a new line.
  * @s: The string to be printed.
  */
 void _puts_recursion(char *s)
